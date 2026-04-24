a5e46721a37c88c64f138d24126e7bb25008c1c8
Krisjscott <krishkumar6566@gmail.com>
2026-04-24 23:18:49 +0530

    Added code and music

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        String filename = "src/music_for_video-in-the-forest-2-21402.wav";
+        File file = new File(filename);
+
+
+        try(AudioInputStream audiostream = AudioSystem.getAudioInputStream(file);
+            Scanner sc = new Scanner(System.in)){
+            String response = " ";
+            Clip clip = AudioSystem.getClip();
+            clip.open(audiostream);
+
+
+            while(!response.equals("Q")){
+
+                System.out.println("P=PLAY");
+                System.out.println("Q=QUIT");
+                System.out.println("R=RESET");
+                System.out.println("S=STOP");
+                System.out.print("Enter a choice: ");
+                response = sc.next().toUpperCase();
+
+                switch(response){
+                    case "P" -> clip.start();
+                    case "Q" -> clip.close();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "S" -> clip.stop();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(IOException e){
+            System.out.println("File Not Found");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println("It's unavailable");
+        }
+        finally{
+            System.out.println("Bye");
+        }
+    }
+}
